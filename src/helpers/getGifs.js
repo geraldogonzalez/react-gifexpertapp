496119ff1357385562d5ce0b0edc924b209511cf
Geraldo Gonzalez <geraldodeveloper09@gmail.com>
2021-04-24 12:28:27 -0400

    Configuradon ruta de API a https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -3,7 +3,7 @@
 export const getGifs = async ({ category }) => {
 
     const key = '2uVpoKNCdP55fYsdQXF3Iz6nt8ma4n8u';
-    const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=${ key }`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=${ key }`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
